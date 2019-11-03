d5bd6aa703accfde2a1aa72e98072c1db37a9877
Daan Van Vooren <daan.vanvooren.y1502@student.hogent.be>
2019-11-03 22:01:52 +0100

    Added Banner, socials and languages

---
+++ sites/default/files/php/twig/5dbf3f5a6931e_forum-list.html.twig_mUuyAjeat_9K-wHNkIuD_sLUb/NrlT73QI4IqyKlIT4Ytib0EVkPNGwceA-5c_Vpv5tZ4.php
@@ -0,0 +1,228 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/classy/templates/dataset/forum-list.html.twig */
+class __TwigTemplate_61f4af7c1224fe4688824d77f0abc9ef008981def7040ce6854d29a42dd56d22 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["for" => 43, "if" => 45];
+        $filters = ["escape" => 33, "t" => 36];
+        $functions = ["range" => 55];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'if'],
+                ['escape', 't'],
+                ['range']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 33
+        echo "<table id=\"forum-";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["forum_id"] ?? null)), "html", null, true);
+        echo "\">
+  <thead>
+    <tr>
+      <th>";
+        // line 36
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Forum"));
+        echo "</th>
+      <th>";
+        // line 37
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Topics"));
+        echo "</th>
+      <th>";
+        // line 38
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Posts"));
+        echo "</th>
+      <th>";
+        // line 39
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Last post"));
+        echo "</th>
+    </tr>
+  </thead>
+  <tbody>
+  ";
+        // line 43
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["forums"] ?? null));
+        foreach ($context['_seq'] as $context["child_id"] => $context["forum"]) {
+            // line 44
+            echo "    <tr id=\"forum-list-";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($context["child_id"]), "html", null, true);
+            echo "\" class=\"";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "zebra", [])), "html", null, true);
+            echo "\">
+      <td ";
+            // line 45
+            if (($this->getAttribute($context["forum"], "is_container", []) == true)) {
+                // line 46
+                echo "colspan=\"4\" class=\"container\"";
+            } else {
+                // line 48
+                echo "class=\"forum-list__forum\"";
+            }
+            // line 49
+            echo ">
+        ";
+            // line 55
+            echo "        ";
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(range(1, $this->getAttribute($context["forum"], "depth", [])));
+            foreach ($context['_seq'] as $context["_key"] => $context["i"]) {
+                if (($this->getAttribute($context["forum"], "depth", []) > 0)) {
+                    echo "<div class=\"indented\">";
+                }
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['i'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 56
+            echo "          <div class=\"forum__icon forum-status-";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "icon_class", [])), "html", null, true);
+            echo "\" title=\"";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "icon_title", [])), "html", null, true);
+            echo "\">
+            <span class=\"visually-hidden\">";
+            // line 57
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "icon_title", [])), "html", null, true);
+            echo "</span>
+          </div>
+          <div class=\"forum__name\"><a href=\"";
+            // line 59
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "link", [])), "html", null, true);
+            echo "\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "label", [])), "html", null, true);
+            echo "</a></div>
+          ";
+            // line 60
+            if ($this->getAttribute($this->getAttribute($context["forum"], "description", []), "value", [])) {
+                // line 61
+                echo "            <div class=\"forum__description\">";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute($context["forum"], "description", []), "value", [])), "html", null, true);
+                echo "</div>
+          ";
+            }
+            // line 63
+            echo "        ";
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(range(1, $this->getAttribute($context["forum"], "depth", [])));
+            foreach ($context['_seq'] as $context["_key"] => $context["i"]) {
+                if (($this->getAttribute($context["forum"], "depth", []) > 0)) {
+                    echo "</div>";
+                }
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['i'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 64
+            echo "      </td>
+      ";
+            // line 65
+            if (($this->getAttribute($context["forum"], "is_container", []) == false)) {
+                // line 66
+                echo "        <td class=\"forum__topics\">
+          ";
+                // line 67
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "num_topics", [])), "html", null, true);
+                echo "
+          ";
+                // line 68
+                if (($this->getAttribute($context["forum"], "new_topics", []) == true)) {
+                    // line 69
+                    echo "            <br />
+            <a href=\"";
+                    // line 70
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "new_url", [])), "html", null, true);
+                    echo "\">";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "new_text", [])), "html", null, true);
+                    echo "</a>
+          ";
+                }
+                // line 72
+                echo "        </td>
+        <td class=\"forum__posts\">";
+                // line 73
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "num_posts", [])), "html", null, true);
+                echo "</td>
+        <td class=\"forum__last-reply\">";
+                // line 74
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["forum"], "last_reply", [])), "html", null, true);
+                echo "</td>
+      ";
+            }
+            // line 76
+            echo "    </tr>
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['child_id'], $context['forum'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 78
+        echo "  </tbody>
+</table>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/dataset/forum-list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  196 => 78,  189 => 76,  184 => 74,  180 => 73,  177 => 72,  170 => 70,  167 => 69,  165 => 68,  161 => 67,  158 => 66,  156 => 65,  153 => 64,  141 => 63,  135 => 61,  133 => 60,  127 => 59,  122 => 57,  115 => 56,  103 => 55,  100 => 49,  97 => 48,  94 => 46,  92 => 45,  85 => 44,  81 => 43,  74 => 39,  70 => 38,  66 => 37,  62 => 36,  55 => 33,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/classy/templates/dataset/forum-list.html.twig", "C:\\xampp\\htdocs\\projecten3\\core\\themes\\classy\\templates\\dataset\\forum-list.html.twig");
+    }
+}
