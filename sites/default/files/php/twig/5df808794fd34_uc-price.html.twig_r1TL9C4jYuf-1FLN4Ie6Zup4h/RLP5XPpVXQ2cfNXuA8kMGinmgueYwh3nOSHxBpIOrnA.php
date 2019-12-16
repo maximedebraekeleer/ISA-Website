956092115217fc5de5686f973360d57b43d422fa
Daan Van Vooren <daan.vanvooren.y1502@student.hogent.be>
2019-12-16 23:54:51 +0100

    Added newspaper + 75 year ISA + translations

---
+++ sites/default/files/php/twig/5df808794fd34_uc-price.html.twig_r1TL9C4jYuf-1FLN4Ie6Zup4h/RLP5XPpVXQ2cfNXuA8kMGinmgueYwh3nOSHxBpIOrnA.php
@@ -0,0 +1,99 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/ubercart/uc_store/templates/uc-price.html.twig */
+class __TwigTemplate_8b4f90bd66220aac204ba78eb3bcf9e46bca0e68bdc5dc66a7fa0177345b7d05 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["spaceless" => 14, "if" => 16];
+        $filters = ["escape" => 15, "join" => 17];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['spaceless', 'if'],
+                ['escape', 'join'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 14
+        ob_start();
+        // line 15
+        echo "<span class=\"uc-price\">";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["price"] ?? null)), "html", null, true);
+        echo "</span>
+";
+        // line 16
+        if ( !twig_test_empty(($context["suffixes"] ?? null))) {
+            // line 17
+            echo "  <span class=\"price-suffixes\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_join_filter($this->sandbox->ensureToStringAllowed(($context["suffixes"] ?? null)), " "), "html", null, true);
+            echo "</span>
+";
+        }
+        echo trim(preg_replace('/>\s+</', '><', ob_get_clean()));
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/ubercart/uc_store/templates/uc-price.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 17,  62 => 16,  57 => 15,  55 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/ubercart/uc_store/templates/uc-price.html.twig", "C:\\xampp\\htdocs\\projecten3\\modules\\ubercart\\uc_store\\templates\\uc-price.html.twig");
+    }
+}
